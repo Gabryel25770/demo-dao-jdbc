932e256cd93de0b7ca36c8ab16e4d81b2596bef8
Gabryel <gabryel-lourenco@hotmail.com>
2025-08-21 23:50:42 -0300

    findById implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,13 +2,12 @@ package model.dao;
 
 import java.util.List;
 
-import model.entities.Department;
 import model.entities.Seller;
 
 public interface SellerDao {
 	void insart(Seller obj);
 	void update(Seller obj);
-	void deleteById(Seller id);
-	Department findById(Seller id);
-	List<Department> findAll();
+	void deleteById(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findAll();
 }
